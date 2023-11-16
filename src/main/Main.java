de3dbc79cd937186378477e3a5ead68df4f69bfa
netuh <wpfn@cin.ufpe.br>
2023-11-16 16:05:03 -0300

    new commit

---
+++ src/main/Main.java
@@ -0,0 +1,7 @@
+import list.EstruturaElementar;
+
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ src/main/list/Pilha.java
@@ -0,0 +1,10 @@
+package list;
+
+public interface Pilha{
+
+    public boolean isEmpty();
+     
+     public void push(int item);
+  
+     public int pop();
+}
